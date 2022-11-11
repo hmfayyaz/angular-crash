1f1d2e546e8a276b74fdcd95115bfcb9a4904041
hmfayyaz <36409793+hmfayyaz@users.noreply.github.com>
2022-11-11 18:22:15 +0500

    observable rxjs

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -12,7 +12,7 @@ export class TaskComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
   }
 
 }
